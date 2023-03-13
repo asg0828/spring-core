f139527ad8a2a4c79946d884a271efd535c9f363
asg0828 <asg0858@gmail.com>
2023-03-14 01:48:32 +0900

    초기 커밋

---
+++ src/test/java/com/example/core/memberrrrr/MemberServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.core.memberrrrr;
+
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1l, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1l);
+
+        //then
+        assertThat(member).isEqualTo(findMember);
+    }
+}
